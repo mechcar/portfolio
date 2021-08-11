3532136fe6c930e9529536188794e9ed73746290
Josh <joshetcarson@gmail.com>
2021-08-11 18:10:49 -0400

    fixed modal on form submission

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -118,7 +118,13 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
-contactForm = document.querySelector("contactForm");
+var contactForm = document.querySelector("form");
+
+var clearForm = function clearForm() {
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("message").value = "";
+};
 
 handleSubmit = function handleSubmit(e) {
   e.preventDefault();
@@ -130,7 +136,24 @@ handleSubmit = function handleSubmit(e) {
     },
     body: new URLSearchParams(formData).toString()
   }).then(function () {
-    console.log("success!");
+    // Get the modal
+    var modal = document.getElementById("messageConfirmation"); // Get the <span> element that closes the modal
+
+    var span = document.getElementsByClassName("close")[0];
+    modal.style.display = "block"; // When the user clicks on <span> (x), close the modal
+
+    span.onclick = function () {
+      modal.style.display = "none";
+    }; // When the user clicks anywhere outside of the modal, close it
+
+
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+
+    clearForm();
   }).catch(function (error) {
     console.log(error.message);
   });
@@ -165,7 +188,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56476" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64974" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
